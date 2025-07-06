a73985b55dbc392cb33cbfafc93f8e5298eec1fe
YourName <your-email@example.com>
2025-07-07 00:33:04 +0100

    Add dotenv and mongoose for environment configuration and MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connet to mongoDb!');    
+})
+ .catch((error)=>{
+    console.log(error);
+    
+ })
 const app =express();
 app.listen(3000,()=>{
     console.log('Server is srunning onport 3000!!!!!uuuu');
