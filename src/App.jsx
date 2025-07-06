5c9e0e226a3839bc2da6c89fe838a564690882c4
YourName <your-email@example.com>
2025-07-06 16:03:17 +0100

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home/>} />
+         <Route path='/sign-up' element={<SignUp/>} />
+         <Route path='/sign-out' element={<SignOut/>} />
+         <Route path='/profile' element={<Profile/>} />
+         <Route path='/about' element={<About/>} />
+
+         
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
