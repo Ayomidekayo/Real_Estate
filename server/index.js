e5e5a45cca1f5e31ca881607412ec177d5878e72
YourName <your-email@example.com>
2025-07-07 04:32:37 +0100

    create  signuoop api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import useRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connet to mongoDb!');    
@@ -10,7 +12,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
     
  })
 const app =express();
+app.use(express.json())
 app.listen(3000,()=>{
     console.log('Server the is srunning onport 3000!!!!!uuuu');
     
 })
+app.use('/api/user',useRouter)
+app.use('/api/auth',authRouter);
