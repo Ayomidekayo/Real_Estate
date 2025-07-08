4fadcacc8354008ae11811e542139d5216d47a92
YourName <your-email@example.com>
2025-07-08 20:53:01 +0100

    complete signin page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
-import SignOut from './pages/SignOut'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './component/Header'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
          <Route path='/' element={<Home/>} />
          <Route path='/sign-up' element={<SignUp/>} />
-         <Route path='/sign-out' element={<SignOut/>} />
+         <Route path='/sign-in' element={<SignIn/>} />
          <Route path='/profile' element={<Profile/>} />
          <Route path='/about' element={<About/>} />
 
